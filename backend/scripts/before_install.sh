77efa0cf7537227c5cd89b4a354a1b1e05009ed9
Abdulla Khalid <abdullak@usc.edu>
2025-06-04 22:52:17 -0700

    Update before_install script

--- backend/scripts/before_install.sh
+++ backend/scripts/before_install.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
 set -e
-
 # Clean up the destination directory
 rm -rf /opt/backend/*
-
-# Wait for CodeDeploy to copy the new files
-sleep 5
-
-# Install Python dependencies
-pip3 install -r /opt/backend/requirements.txt 
