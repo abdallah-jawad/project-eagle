b44b096fdadf5a2990464863af3f7600fe3e0ea0
Abdulla Khalid <khalidll@amazon.com>
2025-06-07 19:00:48 -0700

    Update ec2 ingress rule

--- lib/backend-stack.ts
+++ lib/backend-stack.ts
@@ -28,13 +28,6 @@ export class BackendStack extends cdk.Stack {
       allowAllOutbound: true,
     });
 
-    // Allow inbound HTTP traffic
-    securityGroup.addIngressRule(
-      ec2.Peer.anyIpv4(),
-      ec2.Port.tcp(80),
-      'Allow HTTP traffic'
-    );
-
     // Allow inbound traffic on port 8080
     securityGroup.addIngressRule(
       ec2.Peer.anyIpv4(),
