90d1e3b588e5d8e5a58f58a892789fb054820602
Abdulla Khalid <abdullak@usc.edu>
2025-06-21 22:18:48 -0700

    Fix userData commands for EC2

--- backend/scripts/deployment/before_install.sh
+++ backend/scripts/deployment/before_install.sh
@@ -3,7 +3,5 @@
 rm -rf /opt/backend/*
 
 # Install system packages
-sudo yum update -y
-sudo yum install -y nginx certbot python3-certbot-nginx
 sudo systemctl enable nginx
 sudo systemctl start nginx 
